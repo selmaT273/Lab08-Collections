e571a22dcb0b3d6c535aa93652023c7f1c25aad7
Stacey Teltser <stacey.teltser@gmail.com>
2020-05-13 14:19:24 -0500

    set up project

---
+++ Lab08-Collections/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab08_Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
